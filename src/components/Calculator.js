86a70875b15723aa06233f6e85951b639db2061f
DanielPhilosoph <danielph9@gmail.com>
2021-11-28 21:31:29 +0200

    works but test 13

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -11,74 +11,72 @@ export default function Calculator() {
     setEquationValue("");
   };
 
-  const onNumberClick = (value) => {
-    // If pressed =
-    if (value === "=") {
-      if (equationValue.includes("=")) {
-        setDisplayValue(equationValue.split("=")[1]);
-        setEquationValue(equationValue.split("=")[1]);
+  const onEqualsClick = (value) => {
+    if (equationValue.includes("=")) {
+      setDisplayValue(equationValue.split("=")[1]);
+      setEquationValue(equationValue.split("=")[1]);
+    } else {
+      try {
+        let resault = stringMath(equationValue);
+        setEquationValue(equationValue + value + resault);
+        setDisplayValue(resault);
+      } catch {
+        //? If equationValue isnt a valid equation - Ignore
+      }
+    }
+  };
+
+  const onDotClick = () => {
+    if (!displayValue.includes(".")) {
+      setDisplayValue(displayValue + ".");
+      setEquationValue(equationValue + ".");
+    }
+  };
+
+  const onOperatorClick = (value) => {
+    if (equationValue.includes("=")) {
+      setDisplayValue(value);
+      setEquationValue(equationValue.split("=")[1] + value);
+    } else {
+      if (
+        (displayValue === "*" && value === "-") ||
+        (displayValue === "-" && value === "+")
+      ) {
+        setDisplayValue(value);
+        setEquationValue(equationValue + value);
       } else {
-        try {
-          let resault = stringMath(equationValue);
-          setEquationValue(equationValue + value + resault);
-          setDisplayValue(resault);
-        } catch {
-          //? If equationValue isnt a valid equation - Ignore
+        const reg = new RegExp("^[0-9.]+$");
+        if (!reg.test(displayValue)) {
+          setDisplayValue(value);
+          setEquationValue(
+            equationValue.toString().slice(0, equationValue.length - 1) + value
+          );
+        } else {
+          setDisplayValue(value);
+          setEquationValue(equationValue + value);
         }
       }
+    }
+  };
+
+  const onNumberClick = (value) => {
+    // If = was pressed before
+    if (equationValue.includes("=")) {
+      setDisplayValue(equationValue.split("=")[1] + value);
+      setEquationValue(equationValue.split("=")[1] + value);
     } else {
-      // If = was pressed before
-      if (equationValue.includes("=")) {
+      // If in inisial state
+      if (displayValue === 0 || displayValue === "0") {
         setDisplayValue(value);
-        setEquationValue(equationValue.split("=")[1] + value);
+        setEquationValue(value);
       } else {
-        // If in inisial state
-        if (displayValue === 0 || displayValue === "0") {
-          // If starts with .
-          if (value === ".") {
-            setDisplayValue("0" + value);
-            setEquationValue("0" + value);
-          } else {
-            setDisplayValue(value);
-            setEquationValue(value);
-          }
+        const reg = new RegExp("^[0-9.]+$");
+        if (!reg.test(displayValue)) {
+          setDisplayValue(value);
+          setEquationValue(equationValue + value);
         } else {
-          const reg = new RegExp("^[0-9]+$");
-          // If doesnt have "." init
-          if (!displayValue.toString().includes(".")) {
-            // If number of a "." and not an operator
-            if ((reg.test(value) && reg.test(displayValue)) || value === ".") {
-              setDisplayValue(displayValue + value);
-              setEquationValue(equationValue + value);
-            } else {
-              if (
-                (reg.test(value) && !reg.test(displayValue)) ||
-                (!reg.test(value) && reg.test(displayValue))
-              ) {
-                setDisplayValue(value);
-                setEquationValue(equationValue + value);
-              } else {
-                // If operator is pressed after another oparator is pressed
-                setDisplayValue(value);
-                setEquationValue(
-                  equationValue.toString().slice(0, equationValue.length - 1) +
-                    value
-                );
-              }
-              // If an operator show him only on display
-            }
-          } else if (reg.test(value) && value !== ".") {
-            // If value is a number
-            setDisplayValue(displayValue + value);
-            setEquationValue(equationValue + value);
-          } else {
-            // If value is an operator
-            if (value !== ".") {
-              setDisplayValue(value);
-              setEquationValue(equationValue + value);
-            }
-            // If is a "." - Ignore
-          }
+          setDisplayValue(displayValue + value);
+          setEquationValue(equationValue + value);
         }
       }
     }
@@ -93,30 +91,30 @@ export default function Calculator() {
       <div className="buttons">
         <div>
           <Button id="clear" text="AC" onClick={clear} />
-          <Button id="divide" text="/" onClick={onNumberClick} />
-          <Button id="multiply" text="X" onClick={onNumberClick} />
+          <Button id="divide" text="/" onClick={onOperatorClick} />
+          <Button id="multiply" text="X" onClick={onOperatorClick} />
         </div>
         <div>
           <Button id="seven" text="7" onClick={onNumberClick} />
           <Button id="eight" text="8" onClick={onNumberClick} />
           <Button id="nine" text="9" onClick={onNumberClick} />
-          <Button id="subtract" text="-" onClick={onNumberClick} />
+          <Button id="subtract" text="-" onClick={onOperatorClick} />
         </div>
         <div>
           <Button id="four" text="4" onClick={onNumberClick} />
           <Button id="five" text="5" onClick={onNumberClick} />
           <Button id="six" text="6" onClick={onNumberClick} />
-          <Button id="add" text="+" onClick={onNumberClick} />
+          <Button id="add" text="+" onClick={onOperatorClick} />
         </div>
         <div>
           <Button id="one" text="1" onClick={onNumberClick} />
           <Button id="two" text="2" onClick={onNumberClick} />
           <Button id="three" text="3" onClick={onNumberClick} />
-          <Button id="equals" text="=" onClick={onNumberClick} />
+          <Button id="equals" text="=" onClick={onEqualsClick} />
         </div>
         <div>
           <Button id="zero" text="0" onClick={onNumberClick} />
-          <Button id="decimal" text="." onClick={onNumberClick} />
+          <Button id="decimal" text="." onClick={onDotClick} />
         </div>
       </div>
     </div>
